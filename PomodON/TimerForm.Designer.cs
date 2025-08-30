fa06a1382d40673c198bf5c63599671aa3b8dad2
SionPark <onioni0000002@gmail.com>
2025-08-30 17:07:10 +0900

    Add design draft

--- PomodON/TimerForm.Designer.cs
+++ PomodON/TimerForm.Designer.cs
@@ -28,20 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlDial = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pnlDial.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlDial
+            // 
+            this.pnlDial.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
+            this.pnlDial.BackColor = System.Drawing.Color.Transparent;
+            this.pnlDial.Controls.Add(this.button3);
+            this.pnlDial.Controls.Add(this.button2);
+            this.pnlDial.Controls.Add(this.button1);
+            this.pnlDial.Controls.Add(this.lblTime);
+            this.pnlDial.Location = new System.Drawing.Point(12, 12);
+            this.pnlDial.Name = "pnlDial";
+            this.pnlDial.Size = new System.Drawing.Size(318, 289);
+            this.pnlDial.TabIndex = 0;
+            // 
+            // lblTime
+            // 
+            this.lblTime.BackColor = System.Drawing.Color.MistyRose;
+            this.lblTime.Font = new System.Drawing.Font("MS UI Gothic", 36F);
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(58, 61);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(200, 86);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "label1";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // TimerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 186);
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(342, 313);
+            this.Controls.Add(this.pnlDial);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "TimerForm";
-            this.Text = "Form1";
+            this.Text = "PomodON";
+            this.pnlDial.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlDial;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
